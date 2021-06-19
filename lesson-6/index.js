2d0e548b6f818ab4b35522936d75a0fd0c12fdee
sohagmahin <sohagmahin@gmail.com>
2021-06-19 19:58:43 +0600

    read & write stream

---
+++ lesson-6/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+
+ourReadStream.on('data', (chunk) => {
+    ourWriteStream.write(chunk);
+});
+
+console.log('hello');
