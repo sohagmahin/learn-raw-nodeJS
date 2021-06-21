7bc3f40added1341e0b1a44be36aa80c7b37dd5f
sohagmahin <sohagmahin@gmail.com>
2021-06-21 20:08:16 +0600

    parsing request:sparkles:

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 
 // dependecies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
@@ -27,8 +29,30 @@ app.createServer = () => {
 
 // handle request response
 app.handleReqRes = (req, res) => {
+    // request handling
+    // get the url & parse it
+
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('data', () => {
+        realData += decoder.end();
+        console.log(realData);
+        res.end('hello world');
+    });
+
     // response handle
-    res.end('hello world');
 };
 
 // start the server
