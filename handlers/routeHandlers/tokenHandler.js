a9d99cefb1fa988449403187998afa84cd471f97
sohagmahin <sohagmahin@gmail.com>
2021-07-04 13:29:07 +0600

    CREATE token

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -9,6 +9,7 @@
 
 // dependencies
 const data = require('../../lib/data');
+const { hash, createRandomString, parseJSON } = require('../../helpers/utilities');
 // module scaffolding
 const handler = {};
 
@@ -26,7 +27,64 @@ handler.tokenHandler = (requestProperties, callback) => {
 
 handler._token = {};
 
-handler._token.post = (requestProperties, callback) => {};
+handler._token.post = (requestProperties, callback) => {
+    let phone;
+    let password;
+    // phone number validation check
+    if (
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+    ) {
+        phone = requestProperties.body.phone;
+    } else {
+        phone = false;
+    }
+
+    // password validation check
+    if (
+        typeof requestProperties.body.password === 'string' &&
+        requestProperties.body.password.trim().length > 0
+    ) {
+        password = requestProperties.body.password;
+    } else {
+        password = false;
+    }
+
+    if (phone && password) {
+        data.read('users', phone, (err1, userData) => {
+            const hashedpassword = hash(password);
+            if (hashedpassword === parseJSON(userData).password) {
+                const tokenId = createRandomString(20);
+                // 1 hour expire date
+                const expires = Date.now() + 60 * 60 * 1000;
+                const tokenObject = {
+                    phone,
+                    id: tokenId,
+                    expires,
+                };
+
+                // store tokens to db
+                data.create('tokens', tokenId, tokenObject, (err2) => {
+                    if (!err2) {
+                        callback(200, tokenObject);
+                    } else {
+                        callback(500, {
+                            error: 'There was a problem in server side!',
+                        });
+                    }
+                });
+            } else {
+                callback(400, {
+                    error: 'Password is not valid!',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'You have a problem in your request!',
+        });
+    }
+};
 
 handler._token.get = (requestProperties, callback) => {};
 
--- helpers/utilities.js
+++ helpers/utilities.js
@@ -33,4 +33,24 @@ utilities.hash = (str) => {
     return false;
 };
 
+// create random string
+utilities.createRandomString = (strlength) => {
+    let length = strlength;
+
+    length = typeof strlength === 'number' && strlength > 0 ? strlength : false;
+    if (length) {
+        const possiblecharacters = 'abcdefghigklmnopqrstuvwxyz1234567890';
+        let output = '';
+        for (let i = 1; i <= length; i += 1) {
+            const randomCharacter = possiblecharacters.charAt(
+                // eslint-disable-next-line comma-dangle
+                Math.floor(Math.random() * possiblecharacters.length)
+            );
+            output += randomCharacter;
+        }
+        return output;
+    }
+    return false;
+};
+
 module.exports = utilities;
