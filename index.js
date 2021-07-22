fe644d100eb6f29f9d6f5d8554f73b0b67c8ad0d
sohagmahin <sohagmahin@gmail.com>
2021-07-22 21:14:50 +0600

    refactoring :hammer: the code

--- index.js
+++ index.js
@@ -1,60 +1,28 @@
 /*
- * Title: Uptime Monitoring Application
- * Description: A RESTFul API to monitor up or down time of user defined links
+ * Title: Project initial file
+ * Description: Initial file to start the node server and workers
  * Author: Sohag (www.github.com/sohagmahin)
- * Date: 21/06/2021
+ * Date: 22/07/2021
  *
  */
 
 // dependecies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
-const data = require('./lib/data');
-const { sendTwilioSms } = require('./helpers/notification');
+const server = require('./lib/server');
+const worker = require('./lib/worker');
 
 // app object - module scaffolding
 const app = {};
 
- // ToDo send sms
-// sendTwilioSms('01767566678', 'Hello from sohag!', (err) => {
-//     console.log('The error was :', err);
-// });
+app.init = () => {
+    // start server
+    server.init();
 
-// testing file system
-// @TODO: will be erase
-
-// Write data to file storage
-// data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
-//     console.log(err);
-// });
-
-// read data from file
-// data.read('test', 'newFile', (err, data) => {
-//     console.log(err, data);
-// });
-
-// update the file
-// data.update('test', 'newFile', { name: 'Germany', language: 'German' }, (err) => {
-//     console.log(err);
-// });
-
-// delete file
-// data.delete('test', 'newFile', (err) => {
-//     console.log(err);
-// });
-
-// create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        // console.log(`your environment is ${process.env.NODE_ENV}`);
-        console.log(`listening to port ${environment.port}`);
-    });
+    // start workers
+    worker.init();
 };
 
-// handle reqest and response
-app.handleReqRes = handleReqRes;
+// init the app
+app.init();
 
-// start the server
-app.createServer();
+// export
+module.exports = app;
